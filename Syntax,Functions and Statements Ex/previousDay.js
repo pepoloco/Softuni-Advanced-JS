8cff7a50c847c6c25f990b3f257d2cd02fba77bd
Pepo Banchev <banchevpepo@gmail.com>
2023-05-10 22:30:45 +0300

    ex

---
+++ Syntax,Functions and Statements Ex/previousDay.js
@@ -0,0 +1,6 @@
+function previousDay(year, month, day) {
+  const date = new Date(previousDay());
+  console.log(date);
+  
+}
+previousDay(2016, 9, 30);
---
+++ Syntax,Functions and Statements Ex/sameNumbers.js
@@ -0,0 +1,44 @@
+//forEach
+function sameNum(number) {
+  let numToArr = number.toString().split("");
+  let firstDigit = numToArr[0];
+  let allSame = true;
+
+  numToArr.forEach((digit) => {
+    if (digit !== firstDigit) {
+      allSame = false;
+    }
+  });
+
+  console.log(allSame);
+}
+
+sameNum(2222222);
+sameNum(1234);
+//filer
+function sameNum(number) {
+  let numToArr = number.toString().split("");
+  let firstDigit = numToArr[0];
+  let allSame = numToArr.filter((digit) => digit !== firstDigit).length === 0;
+
+  console.log(allSame);
+}
+
+sameNum(2222222);
+sameNum(1234);
+//reduce
+function sameNum(number) {
+  let numToArr = number.toString().split("");
+  let allSame = numToArr.reduce((acc, digit) => {
+    if (digit !== acc) {
+      return false;
+    } else {
+      return acc;
+    }
+  }, numToArr[0]);
+
+  console.log(allSame);
+}
+
+sameNum(2222222);
+sameNum(1234);
