aaef9314c3707456437d0af6b8a67318bf375e7e
Pepo Banchev <banchevpepo@gmail.com>
2023-05-12 22:30:32 +0300

    updating exercises

--- Syntax,Functions and Statements Ex/previousDay.js
+++ Syntax,Functions and Statements Ex/previousDay.js
@@ -1,6 +1,11 @@
 function previousDay(year, month, day) {
-  const date = new Date(previousDay());
-  console.log(date);
-  
+  let curDay = new Date(year, month - 1, day);
+  curDay.setDate(curDay.getDate() - 1);
+  let newYear = curDay.getFullYear();
+  let newMonth = curDay.getMonth() + 1;
+  let newDate = curDay.getDate();
+
+  return `${newYear}-${newMonth}-${newDate}`;
 }
-previousDay(2016, 9, 30);
+console.log(previousDay(2016, 9, 30));
+console.log(previousDay(2016, 1, 1));
