15e880d54d98e41b8f26a581636a8e3d7696af31
Pepo Banchev <banchevpepo@gmail.com>
2023-05-14 22:28:52 +0300

    ex

--- Syntax,Functions and Statements Ex/fruit.js
+++ Syntax,Functions and Statements Ex/fruit.js
@@ -2,7 +2,7 @@ function calc(...input) {
   let [fruit, weight, price] = input;
   const weightToKg = weight / 1000;
   const money = price * weightToKg;
-  return `I need ${money.toFixed(2)} to buy ${weightToKg.toFixed(
+  return `I need $${money.toFixed(2)} to buy ${weightToKg.toFixed(
     2
   )} kilograms ${fruit}.`;
 }
--- Syntax,Functions and Statements Ex/greatestCommonDivisor.js
+++ Syntax,Functions and Statements Ex/greatestCommonDivisor.js
@@ -1,10 +1,10 @@
-let solve = (num1, num2) => {
+function solve(num1, num2) {
   while (num2) {
     let temp = num1;
     num1 = num2;
     num2 = temp % num2;
   }
   return num1;
-};
+}
 
 console.log(solve(15, 5));
