0badbfc90b519f85cba7e2a5cdd42f7e5a43ccc2
Pepo Banchev <banchevpepo@gmail.com>
2023-06-03 18:13:49 +0300

    ex

---
+++ Advanced Functions/filter-employees.js
@@ -0,0 +1,39 @@
+function solve(input, criteria) {
+  let counter = 0;
+
+  let employees = JSON.parse(input);
+  if (criteria !== "all") {
+    const [propertyName, value] = criteria.split("-");
+    employees = employees.filter((emp) => emp[propertyName] === value);
+  }
+
+  console.log(
+    employees
+      .map((e) => `${counter++}. ${e.first_name} ${e.last_name} - ${e.email}`)
+      .join("\n")
+  );
+}
+
+solve(
+  `[{
+    "id": "1",
+    "first_name": "Ardine",
+    "last_name": "Bassam",
+    "email": "abassam0@cnn.com",
+    "gender": "Female"
+  }, {
+    "id": "2",
+    "first_name": "Kizzee",
+    "last_name": "Jost",
+    "email": "kjost1@forbes.com",
+    "gender": "Female"
+  },  
+{
+    "id": "3",
+    "first_name": "Evanne",
+    "last_name": "Maldin",
+    "email": "emaldin2@hostgator.com",
+    "gender": "Male"
+  }]`,
+  "gender-Female"
+);
