8069f754fe9761f342ce20bb68738363cc64e7a7
Pepo Banchev <banchevpepo@gmail.com>
2023-09-13 23:57:25 +0300

    Update EditElement.js

--- DOM Introduction/01.Edit Element/EditElement.js
+++ DOM Introduction/01.Edit Element/EditElement.js
@@ -2,6 +2,6 @@ function editElement(reference, match, replacer) {
   let text = reference.textContent;
   let regEx = new RegExp(match, "g")
   let res = text.replace(regEx,replacer)
-  text.textContent(res)
+  reference.textContent = res
 }
 
