f4c499371c81922e3cd2802d1011eace56fd30b0
Pepo Banchev <banchevpepo@gmail.com>
2023-09-24 23:54:32 +0300

    ex

--- DOM Introduction - Exercise/05. Table - Search Engine/solution.js
+++ DOM Introduction - Exercise/05. Table - Search Engine/solution.js
@@ -1,3 +1,16 @@
 function solve() {
-
+    let columns = document.querySelectorAll('tbody tr');
+    document.getElementById('searchBtn').addEventListener('click', onClick);
+    function onClick(){
+        let searchField = document.getElementById('searchField').value.toLowerCase();
+        for (const row of columns) {
+            const textToLower = row.textContent.toLowerCase();
+            if(textToLower.includes(searchField)){
+                row.setAttribute("class","select");
+            }else{
+                row.removeAttribute('class')
+            }
+        }
+    }
+   
 }
