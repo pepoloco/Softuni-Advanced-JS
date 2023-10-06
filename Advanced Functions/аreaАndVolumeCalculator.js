d17df5c42d6dc7f388a0a57706c97c5e0bb49baa
Pepo Banchev <banchevpepo@gmail.com>
2023-10-06 23:41:24 +0300

    ex

--- Advanced Functions/аreaАndVolumeCalculator.js
+++ Advanced Functions/аreaАndVolumeCalculator.js
@@ -5,14 +5,14 @@ function vol() {
   return Math.abs(this.x * this.y * this.z);
 }
 function solve(area, vol, input) {
+  let parsed = JSON.parse(input);
   let result = [];
-  let objects = JSON.parse(input);
-  for (const obj of objects) {
-    let areaValue = area.call(obj);
-    let volume = vol.call(obj);
-    result.push({ area: areaValue, volume });
+  for (const el of parsed) {
+    let areaValue = area.call(el);
+    let volValue = vol.call(el);
+    result.push({ area: areaValue, volume: vol });
   }
-  return result;
+  console.log(result);
 }
 
 let input = `[
